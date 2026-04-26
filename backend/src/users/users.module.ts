9523a0bbab34923ecde14453c368a972d08f27e4
mekind <mekind98@gmail.com>
2026-04-26 16:27:17 +0900

    feat(backend): BE-22 implement POST /users/:id/log

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { RepositoriesModule } from '../repositories/repositories.module';
 import { UsersController } from './users.controller';
+import { UsersInternalController } from './users-internal.controller';
 import { UsersService } from './users.service';
 
 @Module({
   imports: [RepositoriesModule],
-  controllers: [UsersController],
+  controllers: [UsersController, UsersInternalController],
   providers: [UsersService],
 })
 export class UsersModule {}
